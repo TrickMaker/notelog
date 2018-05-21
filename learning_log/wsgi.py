5d1859ea0e82e1b6f96cbfeec3a43cc2f925ec7b
huhong <670351312@qq.com>
2018-05-21 21:31:46 +0800

    Change python version

--- learning_log/wsgi.py
+++ learning_log/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
-
 application = Cling(get_wsgi_application())
